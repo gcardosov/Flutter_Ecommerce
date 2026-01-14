f6941a658b49d55d82d717f1e7004af55e2d4124
Gerardo Cardoso <ger9047@gmail.com>
2026-01-14 01:20:44 -0600

    Terminando diseño de pantalla de login

--- lib/src/presentation/pages/auth/login/LoginPage.dart
+++ lib/src/presentation/pages/auth/login/LoginPage.dart
@@ -5,6 +5,145 @@ class Loginpage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: Text('LoginPage')));
+    return Scaffold(
+      body: Container(
+        width: double.infinity,
+        child: Stack(
+          alignment: Alignment.center,
+          children: [
+            Image.asset(
+              'assets/img/background1.jpg',
+              width: MediaQuery.of(context).size.height,
+              height: MediaQuery.of(context).size.height,
+              fit: BoxFit.cover,
+              color: Colors.black54,
+              colorBlendMode: BlendMode.darken,
+            ),
+
+            Container(
+              width: MediaQuery.of(context).size.width * 0.85,
+              height: MediaQuery.of(context).size.height * 0.75,
+              decoration: BoxDecoration(
+                color: Color.fromRGBO(255, 255, 255, 0.3),
+                borderRadius: BorderRadius.all(Radius.circular(25)),
+              ),
+
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Icon(Icons.person, color: Colors.white, size: 125),
+                  Text(
+                    'LOGIN',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+
+                  Container(
+                    margin: EdgeInsets.only(left: 25, right: 25),
+                    child: TextField(
+                      decoration: InputDecoration(
+                        label: Text(
+                          'Correo electronico',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                        prefixIcon: Icon(Icons.email, color: Colors.white),
+                        enabledBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(color: Colors.white),
+                        ),
+                        focusedBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(color: Colors.white),
+                        ),
+                      ),
+                    ),
+                  ),
+
+                  Container(
+                    margin: EdgeInsets.only(left: 25, right: 25),
+                    child: TextField(
+                      decoration: InputDecoration(
+                        label: Text(
+                          'Contraseña',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                        prefixIcon: Icon(Icons.lock, color: Colors.white),
+
+                        enabledBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(color: Colors.white),
+                        ),
+                        focusedBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(
+                            color: const Color.fromRGBO(255, 255, 255, 1),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+
+                  Container(
+                    width: MediaQuery.of(context).size.width,
+                    margin: EdgeInsets.only(
+                      left: 25,
+                      right: 25,
+                      top: 25,
+                      bottom: 15,
+                    ),
+                    height: 55,
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.green,
+                      ),
+                      child: Text(
+                        'INICIAR SESION',
+                        style: TextStyle(color: Colors.black),
+                      ),
+                    ),
+                  ),
+
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Container(width: 65, height: 1, color: Colors.white),
+                      //SizedBox(width: 10),
+                      Text(
+                        '¿No tienes cuenta?',
+                        style: TextStyle(color: Colors.white, fontSize: 18),
+                      ),
+                      //SizedBox(width: 10),
+                      Container(width: 65, height: 1, color: Colors.white),
+                    ],
+                  ),
+
+                  Container(
+                    width: MediaQuery.of(context).size.width,
+                    margin: EdgeInsets.only(
+                      left: 25,
+                      right: 25,
+                      top: 15,
+                      bottom: 15,
+                    ),
+                    height: 55,
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.black,
+                      ),
+                      child: Text(
+                        'REGISTRATE',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
